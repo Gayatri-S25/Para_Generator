88301d2ddabb7bae11d137d5f6044880c2bb41e1
Gayatri-S25 <shingategayatri45@gmail.com>
2024-03-15 16:27:05 +0530

    My First Commit

---
+++ script.js
@@ -0,0 +1,37 @@
+const input=document.querySelector('#input')
+const button=document.querySelector('.btn')
+const container=document.querySelector('.container');
+
+
+
+const generateWord=(n)=>{
+    const letters="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    let word="";
+    for(let i=0;i<n;i++){
+        let random=(Math.random()*25).toFixed(0);
+        word+=letters[random];
+    }
+   return word;
+}
+
+let num;
+const generatePara=()=>{
+   num=input.value;
+    
+    const para = document.createElement('p');
+    para.setAttribute("class","paras");
+
+    let data="";
+    for(let i=0;i<num;i++){
+        data+=generateWord((Math.random()*10).toFixed(0));
+        data+=" ";
+    }
+    para.innerText=data;
+    container.append(para);
+}
+
+
+
+
+
+button.addEventListener('click',generatePara);
